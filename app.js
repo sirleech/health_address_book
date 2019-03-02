fd5eac5f63ee7ddb70710a25975477e5a8fdb205
Chris Lee <christopher.lee@industry.gov.au>
2019-03-03 09:38:55 +1100

    First sort of patients by ward

--- app.js
+++ app.js
@@ -2,11 +2,43 @@ fetch('address-book.json')
   .then(function(response) {
     return response.json();
   })
-  .then(function(myJson) {
-    const address_book_data = JSON.stringify(myJson);
-    render_page(address_book_data);
+  .then(function(json) {
+    render_page(json);
   });
 
-function render_page(address_book_data) {
-  console.log(address_book_data);
+function render_page(json) {
+
+  let patients = new Array();
+  let nurses = new Array();
+  let doctors = new Array();
+
+  for (i = 0; i < 11 ; i++) {
+    patients[i] = new Object();
+    patients[i].wardNumber = i;
+    patients[i].wardPhoneNumber = "";
+    patients[i].people = new Array();
+
+    nurses[i] = new Object();
+    nurses[i].wardNumber = i;
+    nurses[i].wardPhoneNumber = "";
+    nurses[i].people = new Array();
+  }
+
+  for (i = 0; i < json.length; i++) {
+    if (json[i].class == "Patient") {
+      if (json[i].ward_number != null) {
+        patients[json[i].ward_number].people.push(json[i].first_name + " " + json[i].family_name);
+      } else {
+        patients[0].people.push(json[i].first_name + " " + json[i].family_name);
+      }
+    }
+  }
+
+  // render patients list
+  for (i = 0; i < patients.length; i++) {
+    console.log(patients[i].wardNumber);
+    for (j = 0; j < patients[i].people.length; j++) {
+      console.log(patients[i].people[j]);
+    }
+  }
 }
